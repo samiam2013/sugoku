655522b95ca8a6c7e455df7c64eba79718642e2c
samiam2013 <samiam2013@gmail.com>
2026-06-30 07:37:32 -0500

    add validation logic

--- main.go
+++ main.go
@@ -6,17 +6,79 @@ import (
 	"log/slog"
 	"os/exec"
 	"strconv"
+	"time"
 )
+
+func main() {
+	brd := generateBoard(3)
+	// fmt.Printf("%#v\n", brd)
+	fmt.Printf("is board valid?: %t\n", brd.validate())
+}
 
 type board struct {
 	box [81]int
 }
 
-func main() {
-	fmt.Printf("%#v\n", generateBoard(30))
+func (b *board) validate() bool {
+	startT := time.Now()
+	// check each line
+	// fmt.Println("checking each line")
+	for i := range 9 {
+		if !validateSeries(b.box[(i * 9):((i * 9) + 9)]) {
+			return false
+		}
+	}
+	// check each column
+	// fmt.Println("checking each column")
+	for i := range 9 {
+		col := [9]int{}
+		for j := range 9 {
+			col[j] = b.box[i+(j*9)]
+		}
+		if !validateSeries(col[:]) {
+			return false
+		}
+	}
+	// check each house
+	// fmt.Println("checking each house")
+	for i := range 3 {
+		for j := range 3 {
+			house := []int{}
+			for k := range 3 {
+				startIdx := (k * 9) + (j * 9) + (i * 3)
+				endIdx := startIdx + 3
+				house = append(house, b.box[startIdx:endIdx]...)
+			}
+			if !validateSeries(house) {
+				return false
+			}
+		}
+	}
+	fmt.Printf("validated in %s\n", time.Since(startT))
+	return true
+}
+
+func validateSeries(nineBoxes []int) bool {
+	if len(nineBoxes) < 9 {
+		panic("less than 9 boxes in series to validate!")
+	}
+	if len(nineBoxes) > 9 {
+		panic("more than 9 boxes in series to validate!")
+	}
+	freq := make(map[int]int, 9)
+	for _, val := range nineBoxes {
+		if f, ok := freq[val]; !ok {
+			freq[val] = 1
+		} else if f > 1 && val != 0 {
+			return false
+		}
+	}
+	// fmt.Printf("%#v\n", freq)
+	return true
 }
 
 func generateBoard(emptyCount int) board {
+	startT := time.Now()
 	cmd := exec.Command("python3", "./g4gGenerator/main.py", strconv.Itoa(emptyCount))
 	b, err := cmd.Output()
 	if err != nil {
@@ -35,5 +97,6 @@ func generateBoard(emptyCount int) board {
 			i++
 		}
 	}
+	fmt.Printf("board generation took %s\n", time.Since(startT))
 	return brd
 }
